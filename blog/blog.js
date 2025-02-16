f15e41d0271fc4aac850eab5db8f981e020125a4
JamesGassaway <jamgass329@gmail.com>
2025-02-15 20:39:42 -0700

    blog update 2

--- blog/blog.js
+++ blog/blog.js
@@ -22,6 +22,73 @@ const articles = [
 		imgAlt: 'Book cover for Magnus Chase 1',
 		ages: '12-16',
 		genre: 'Fantasy',
-		stars: '⭐⭐⭐⭐'
+		stars: '⭐⭐⭐⭐',
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+
+let parent = document.getElementById("article-container");
+let referenceElement = document.getElementById("filters");
+
+for (let article = 0; article < articles.length; article++) {
+	const articleDetails = document.createElement("div");
+	articleDetails.setAttribute('class', 'article-details')
+
+	const newDate = document.createElement("p");
+	newDate.innerText = articles[article].date;
+	newDate.setAttribute('class', 'date')
+	articleDetails.appendChild(newDate);
+
+	const newAges = document.createElement("p");
+	newAges.innerText = articles[article].ages;
+	newAges.setAttribute('class', 'ages')
+	articleDetails.appendChild(newAges);
+
+	const newGenre = document.createElement("p");
+	newGenre.innerText = articles[article].genre;
+	newGenre.setAttribute('class', 'genre')
+	articleDetails.appendChild(newGenre);
+	
+	const newRating = document.createElement("p");
+	newRating.innerText = articles[article].stars;
+	newRating.setAttribute('class', 'rating')
+	articleDetails.appendChild(newRating);
+
+	const articlePreview = document.createElement("div");
+	articlePreview.setAttribute('class', 'article-preview')
+
+	const newTitle = document.createElement("h2");
+	newTitle.innerText = articles[article].title;
+	newTitle.setAttribute('class', 'title')
+	articlePreview.appendChild(newTitle);
+
+	const image = document.createElement('img');
+	image.setAttribute('src', articles[article].imgSrc)
+	image.setAttribute('alt', articles[article].imgAlt);
+	articlePreview.appendChild(image);
+
+	const newDescription = document.createElement("p");
+	newDescription.innerText = articles[article].description;
+	newDescription.setAttribute('class', 'rating')
+	articlePreview.appendChild(newDescription);
+
+	//parent.appendChild(articleDetails);
+	//parent.appendChild(articlePreview);
+
+	// Insert the new element after the reference element
+
+    parent.insertBefore(articleDetails, referenceElement);
+	parent.insertBefore(articlePreview, referenceElement);
+}
